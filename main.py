3ac741a36259826d73995ccb4ded7dd9148aefd5
Marshall Dork <76794680+HiddenDork@users.noreply.github.com>
2023-10-09 22:10:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from pyrogram import Client,filters
 import pyrogram
 plugins = dict(root="plugins")
 
-Client("boty",15102119,'3dfdcee3e3bedad4738f81287268180f',bot_token='Token', plugins=plugins, parse_mode=pyrogram.enums.ParseMode.HTML).run()
+Client("boty",15102119,'3dfdcee3e3bedad4738f81287268180f',bot_token='6644409337:AAH7wI-B2gIHZzp2AuKfpaMH7Ky6X1PkcMs', plugins=plugins, parse_mode=pyrogram.enums.ParseMode.HTML).run()
